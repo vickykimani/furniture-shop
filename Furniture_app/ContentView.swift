dc8dc3ff7f826c92653bca2b0f0899497b24ba6a
vickykimani <victorianightingale70@gmail.com>
2023-02-02 12:57:25 +0300

    extracted view

--- Furniture_app/ContentView.swift
+++ Furniture_app/ContentView.swift
@@ -9,14 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-        
+        ZStack{
+            Color("Bg")
+                .ignoresSafeArea()
+            
+            VStack{
+                AppBarView()
+            }
+            
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct AppBarView: View {
+    var body: some View {
+        HStack{
+            Button(action: {}) {
+                Image("menu")
+                    .padding()
+                    .background(Color(.white))
+                    .cornerRadius(10.0)
+            }
+            
+            Spacer()
+            
+            Button(action: {}) {
+                Image("Profile")
+                    .resizable()
+                    .frame(width: 42, height: 42)
+                    .cornerRadius(10.0)
+            }
+            
+            
+        }
+        .padding(.horizontal)
     }
 }
